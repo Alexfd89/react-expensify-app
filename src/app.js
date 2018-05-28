690fa143d2ac393e94ee8e110929555a81e304b5
Alex Fadeev <alexfd89@gmail.com>
2018-05-28 10:09:59 +0300

    Setup production webpack build

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ import {addExpense, removeExpense, editExpense} from './actions/expenses';
 import {setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate} from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
 
+console.log('test');
 const store = configureStore();
 
 //Adding Expenses.
