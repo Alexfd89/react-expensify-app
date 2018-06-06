905c01ae5290e38d524bdd0aa3b81d165952e5ca
Alex Fadeev <alexfd89@gmail.com>
2018-06-06 19:09:19 +0300

    fb login method

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { startLogin } from '../actions/auth';
+import { startLogin, startFacebookLogin } from '../actions/auth';
 
-export const LoginPage = ({ startLogin }) => (
+export const LoginPage = ({ startLogin, startFacebookLogin }) => (
     <div className="box-layout">
         <div className="box-layout__box">
             <h1 className="box-layout__title">Expensify</h1>
             <p>It's time to get your expenses under control.</p>
             <img src="./images/sign-in-button.png" className="login_button" onClick={startLogin} />
+            <button onClick={startFacebookLogin}>Login with facebook</button> 
         </div>
     </div>
 );
 
 const mapDispatchToProps = (dispatch) => ({
-    startLogin: () => dispatch(startLogin())
+    startLogin: () => dispatch(startLogin()),
+    startFacebookLogin: () => dispatch(startFacebookLogin())
 });
 
 export default connect(undefined, mapDispatchToProps)(LoginPage);
